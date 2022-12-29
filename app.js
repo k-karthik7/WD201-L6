45466a90ffca3478c315a325b4dab1cd710a51ae
k-karthik7 <kkotha79@gmail.com>
2022-12-30 00:12:44 +0530

    level 7 modification

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const path = require("path")
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
-});
+// app.get("/", function (request, response) {
+//   response.send("Hello World");
+// });
+
+app.set("view engine","ejs");
+
+app.get("/",async(request , response)=>{
+  const allTodos=await Todo.getTodos;
+  if (request.accepts("html")){
+    response.render('index' ,{
+      allTodos
+    });
+  }else{
+    response.json({
+      allTodos
+    })
+  }
+  
+})
+
+app.use(express.static(path.join(__dirname , 'public')));
+
+app.get("/todos",(request, response)=>{
+  console.log("Todo List",request.body)
+})
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
