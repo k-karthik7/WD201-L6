663898aab3dcf3c4a322636cd6f1227656215ee4
k-karthik7 <kkotha79@gmail.com>
2023-01-03 00:28:57 +0530

    views changed

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
 const path = require("path")
 
+app.use(bodyParser.json());
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname , 'public')));
 
 app.get("/",async(request , response)=>{
   const allTodos=await Todo.getTodos();
@@ -20,14 +21,12 @@ app.get("/",async(request , response)=>{
       allTodos
     });
   }else{
-    response.json({
-      allTodos
-    })
+    response.json(allTodos);
   }
   
 })
 
-app.use(express.static(path.join(__dirname , 'public')));
+
 
 app.get("/todos",(request, response)=>{
   console.log("Todo List",request.body)
